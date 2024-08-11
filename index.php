be972040246b521ce1ad4a2aa2aeeae9cab0010d
masudcse92 <masudcse9292@gmail.com>
2024-08-11 20:04:53 +0600

    php learn start

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
     <title>Learn basic PHP</title>
 </head>
 <body>
-    
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta, natus. Odit, maxime dicta, consequatur, sed eaque velit quod magni reprehenderit reiciendis laboriosam nihil nostrum eligendi cum hic neque accusantium illo.</p>
 </body>
 </html>
