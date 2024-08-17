a9341ce4026f931f4b6cfa91e7fda604a1063403
masudcse92 <masudcse9292@gmail.com>
2024-08-17 23:48:52 +0600

    function

---
+++ common.php
@@ -0,0 +1,12 @@
+<?php
+
+function price_cal(){
+    $product_price = 100;
+    $product_qty = 1;
+    $total_price  = $product_price * $product_qty;
+    return $total_price;
+}
+
+
+
+?>
---
+++ return.php
@@ -0,0 +1,8 @@
+<?php
+
+include('common.php');
+
+echo price_cal();
+
+
+?>
