58e3e6df71ea4217927e733e89c2e3b28edeeace
masudcse92 <masudcse9292@gmail.com>
2024-08-18 00:51:22 +0600

    Multidimension array

---
+++ associative_array.php
@@ -0,0 +1,15 @@
+<?php
+
+$info = array (
+    'name' => "Masud Rana",
+    'cell' => "0197483377",
+    'address' => 'Mirpur-10'
+);
+
+// echo $info['address'];
+
+foreach($info as $key => $value ){
+    echo $key = $value ."</br>";
+}
+
+?>
---
+++ multidimensions_array.php
@@ -0,0 +1,18 @@
+<?php
+
+$info = array(
+    "name" => "Masud Rana",
+    "address" => array(
+        "village" => "Batikamari",
+        "union" => "Mollarchore",
+        "zila" => "Gaibandha sadar"
+    ),
+    "phone" => "013488338"
+);
+
+// echo var_dump($info);
+
+echo $info["address"]["zila"];
+
+
+?>
