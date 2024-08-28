fa55a11c8ccc9f0fe654cc1c09c534c632dcf3e3
masudcse92 <masudcse9292@gmail.com>
2024-08-28 21:48:04 +0600

    Global variable

---
+++ request.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>User Registration</h1>
+<form action="" method="post">
+    <label for="Username">User Name</label>
+    <input type="text" name="username">
+    <button type="submit">Submit</button>
+</form>
+    
+</body>
+</html>
