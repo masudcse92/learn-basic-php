05c980f342812a58697e152dbc4537de46b15fe2
masudcse92 <masudcse9292@gmail.com>
2024-08-28 23:37:33 +0600

    GET Post ID

---
+++ parameter.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="update.php?msg=Welcome to PHP Learn">Update</a>
+    
+</body>
+</html>
